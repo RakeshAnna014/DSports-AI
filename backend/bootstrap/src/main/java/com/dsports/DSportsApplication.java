c5fe4f4c145ae9a54428944b9fe79049c14649a1
ranna <ranna02@safeway.com>
2026-07-11 10:08:27 +0530

    Added backend dependencies

--- backend/bootstrap/src/main/java/com/dsports/DSportsApplication.java
+++ backend/bootstrap/src/main/java/com/dsports/DSportsApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.dsports")
 @EnableR2dbcRepositories(basePackages = "com.dsports")
 public class DSportsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DSportsApplication.class, args);
     }
+
 }
